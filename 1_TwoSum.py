dd3f0a17bbede7d8046be932a7e92acb1cd45be0
James Lucas <56455079+JamesLucas943@users.noreply.github.com>
2023-11-14 01:45:54 -0700

    Create 1_TwoSum.py

---
+++ 1_TwoSum.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i, len(nums)-1):
+                if(nums[i]+nums[j+1] == target):return [i,j+1]
